a28057bea0de4d85bcfef7c3236ffcffa443a0f7
Philip Wisner <philipwisner@gmail.com>
2024-05-08 22:48:29 -0400

    alt card design

--- src/components/CardGroup.tsx
+++ src/components/CardGroup.tsx
@@ -15,12 +15,29 @@ interface Props {
 function CardGroup(props: Props) {
   return (
     <>
+      {/* <a
+        href="#"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="font-normal text-gray-700 ">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </a> */}
+
       <h3>{props.heading}</h3>
       <ul className="cards-container">
         {props.cards.map((card) => {
           return (
-            <li key={card.value} className="card-container">
-              <a href={card.link} target="_blank">
+            <li key={card.value} className="card group">
+              <a
+                href={card.link}
+                className="card-image h-full group-hover:h-auto"
+                target="_blank"
+              >
                 <Image
                   className="card-img-top img-responsive"
                   src={card.image}
@@ -29,10 +46,16 @@ function CardGroup(props: Props) {
                   height={200}
                 />
               </a>
-              <a href={card.link} target="_blank">
-                {card.name}
-              </a>
-              <p className="card-text">{card.description}</p>
+              <div className="bg-gray-50 p-4 text-center w-full hidden group-hover:block">
+                <a
+                  href={card.link}
+                  target="_blank"
+                  className="font-bold text-xl text-gray-900"
+                >
+                  {card.name}
+                </a>
+                <p className="text-base mt-2">{card.description}</p>
+              </div>
             </li>
           );
         })}
